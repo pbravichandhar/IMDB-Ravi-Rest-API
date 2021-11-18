1f2b544274d83bda2440e7759f9730f03d56c7e7
pbravichandhar <ravifabulous471@gmail.com>
2021-11-18 19:13:08 +0530

    Route order changed

--- routes/movie.route.js
+++ routes/movie.route.js
@@ -5,11 +5,11 @@ const { addMovie, setFavouriteGeneres, getRecommendedMovies, voteMovie, reviewMo
 const { createMovieValidation, getMovieByIdValidation, setFavouriteValidation, voteMovieValidation, reviewMovieValidation, getAllMoviesValidation} = require('../validators/movie.validator')
 const { validate } = require('../validators/common.validator')
 
+router.get('/recommendations', getRecommendedMovies);
 router.post('/', createMovieValidation(), validate, addMovie);
 router.get('/all', getAllMoviesValidation(), validate, getAllMovies);
 router.get('/:movieId', getMovieByIdValidation(), validate, getMovieById);
 router.put('/genre/favourite', setFavouriteValidation(), validate, setFavouriteGeneres);
-router.get('/recommendations', getRecommendedMovies);
 router.put('/vote', voteMovieValidation(), validate, voteMovie);
 router.put('/review', reviewMovieValidation(), validate, reviewMovie);
 
